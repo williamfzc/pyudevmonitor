48a8f2556ab9421064aacd9fedfdb12870a0e5be
williamfzc <178894043@qq.com>
2019-11-07 14:56:36 +0800

    chore: all the possible args listed in UEvent

--- pyudevmonitor/event.py
+++ pyudevmonitor/event.py
@@ -7,6 +7,37 @@ class UEvent(object):
     """ convert udev info (str) into object """
 
     def __init__(self, udev_info_list: typing.List[str]):
+        # args
+        self.ACTION: str = ""
+        self.DEVPATH: str = ""
+        self.DEVTYPE: str = ""
+        self.DRIVER: str = ""
+        self.ID_BUS: str = ""
+        self.ID_FOR_SEAT: str = ""
+        self.ID_MODEL: str = ""
+        self.ID_MODEL_ID: str = ""
+        self.ID_PATH: str = ""
+        self.ID_PATH_TAG: str = ""
+        self.ID_REVISION: str = ""
+        self.ID_SERIAL: str = ""
+        self.ID_SERIAL_SHORT: str = ""
+        self.ID_USB_INTERFACES: str = ""
+        self.ID_VENDOR: str = ""
+        self.ID_VENDOR_ENC: str = ""
+        self.ID_VENDOR_FROM_DATABASE: str = ""
+        self.ID_VENDOR_ID: str = ""
+        self.INTERFACE: str = ""
+        self.MAJOR: str = ""
+        self.MINOR: str = ""
+        self.MODALIAS: str = ""
+        self.PRODUCT: str = ""
+        self.SEQNUM: str = ""
+        self.SUBSYSTEM: str = ""
+        self.TAGS: str = ""
+        self.TYPE: str = ""
+        self.USEC_INITIALIZED: str = ""
+        self.adb_user: str = ""
+
         # empty event
         self._empty: bool = False
         if not udev_info_list:
