1508f7cdc1d6f9742822564ad7244ef10a2a59c1
William Feng <178894043@qq.com>
2019-11-06 19:15:11 -0800

    feat(event): start and end log

--- pyudevmonitor/event.py
+++ pyudevmonitor/event.py
@@ -7,10 +7,15 @@ class UEvent(object):
     """ convert udev info (str) into object """
 
     def __init__(self, udev_info_list: typing.List[str]):
+        if not udev_info_list:
+            logger.warning("empty udev event")
+            return
+        logger.info("udev event start")
         for each_arg in udev_info_list[1:]:
             logger.debug(each_arg)
             name, value = each_arg.split("=")
             self.__dict__[name] = value
+        logger.info("udev event end")
 
     @property
     def desc(self) -> str:
