b24b7ac2213b9266e7c3dc4a0e4f29a9db6fe41c
williamfzc <178894043@qq.com>
2019-11-07 11:45:13 +0800

    docs: full example and doc

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pyudevmonitor
+
 simple wrapper of udevadm monitor
+
+## firstly
+
+you should notice that, because of udevadm, **this repo only works on Linux.**
+
+## installation
+
+```bash
+pip install pyudevmonitor
+```
+
+## usage
+
+see [demo.py](demo.py)
+
+## licencing
+
+[MIT](LICENSE)
--- demo.py
+++ demo.py
@@ -1,7 +1,28 @@
 from pyudevmonitor.monitor import UDevMonitor
 
-
+# init
 m = UDevMonitor()
 m.start()
+
+# read single event
 e = m.read_event()
 print(e.desc)
+
+# or, start a detector
+# init a queue
+import queue
+q = queue.Queue()
+# and bind it to monitor
+# monitor will put new UEvent to queue
+stop = m.loop_read(q)
+
+# do some operations ... ( plug in and out )
+import time
+time.sleep(10)
+
+# stop detector
+stop()
+
+# you will get series of UEvent object by order
+for each in list(q.queue):
+    print(each)
